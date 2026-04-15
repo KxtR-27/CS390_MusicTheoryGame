18635bc8de8dc71e2ec3701545455f5e942a38ea
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-04-15 13:38:43 -0400

    Tweak notes in main.gd

--- project/scripts/main.gd
+++ project/scripts/main.gd
@@ -2,9 +2,6 @@ class_name Main
 extends Node2D
 
 
-# variables
-
-
 func _ready() -> void:
 	pass
 
@@ -15,8 +12,9 @@ func _process(_delta: float) -> void:
 
 # Notes:
 #
-# Player win condition in `enemy.gd#_on_battle_menu_navigation_attack_enemy()`
-# Player loss condition in `player_attack.gd#Enter()`
+# win/loss triggers
+# -> Player win condition in `enemy.gd#_on_battle_menu_navigation_attack_enemy()`
+# -> Player loss condition in `player_attack.gd#Enter()`
 #
 # Sequencer does not work so `player_attack.gd#_use_sequence()` returns hardcoded value
 # -> this applies to player attacks AND player defense
